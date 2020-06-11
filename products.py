e875d561673b4c6227011a6238ca309418b0c465
Zairong <justiceman3211@hotmail.com>
2020-06-11 20:14:10 +0800

    新增確認檔案和讀取功能

--- products.py
+++ products.py
@@ -1,4 +1,17 @@
+import os
 products = []
+if os.path.isfile('products.csv'):
+	print('有此檔案')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)	
+else:
+	print('無此檔案')
+		
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
@@ -8,7 +21,7 @@ while True:
 print(products)
 for p in products:
 	print(p[0], '的價格是', p[1], '$')
-with open ('products.csv', 'w', encoding = 'utf-8') as f:
+with open ('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n' )
